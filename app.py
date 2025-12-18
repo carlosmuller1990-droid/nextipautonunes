79adfa05749f4ebebdb6e5b2c2c1d5f98b05dd4f
carlosmuller1990-droid <carlos.muller1990@gmail.com>
2025-12-17 22:06:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.markdown(
 import streamlit as st
 
 st.image(
-    "https://raw.githubusercontent.com/SEU_USUARIO/SEU_REPOSITORIO/main/images/Captura%20de%20tela%202025-12-17%20214740.png",
+    "https://raw.githubusercontent.com/carlosmuller1990-droide/nextipautonunes/main/imagemdoprint",
     caption="Exemplo de planilha no formato correto",
     use_container_width=True
 )
